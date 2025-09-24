3add3b7661fff78e5298711668d940a03c9b8859
MD ALIF MIA <alifhossinsajjad123456@gmail.com>
2025-09-24 23:12:01 +0600

    functionality done

--- src/App.jsx
+++ src/App.jsx
@@ -3,35 +3,28 @@
 import './App.css'
 import Container from './components/container/Container'
 import Buttons from './components/Buttons/Buttons'
-import { Suspense, useState } from 'react'
+import { Suspense, } from 'react'
 import CountBox from './components/CountBox/CountBox'
 import Assignment from './components/Assingment/Assignment'
 
-const assignmentPromise = fetch('/utilits/data.json').then(res=> res.json());
-
+const assignmentPromise = fetch('/utilits/data.json').then(res => res.json());
 
 function App() {
 
-  const [toggleStatus, setToggleStatus] = useState("All")
+
 
 
   return (
-    
-<div>
+
+    <div>
 
 
-<CountBox/>
-
-
-
-<Buttons toggleStatus = {toggleStatus} setToggleStatus={setToggleStatus}/>
-
-{
-  <Suspense fallback= { <p>Loading......</p> }>
-    <Assignment assignmentPromise = {assignmentPromise}/>
-  </Suspense>
-}
-</div>
+      {
+        <Suspense fallback={<p>Loading......</p>}>
+          <Assignment assignmentPromise={assignmentPromise} />
+        </Suspense>
+      }
+    </div>
 
 
   )
--- src/components/Assingment/Assignment.jsx
+++ src/components/Assingment/Assignment.jsx
@@ -1,30 +1,50 @@
-import React, { use } from 'react';
-import userImg from '../../assets/heroImage.png'
+import React, { Suspense, use, useState } from 'react';
 
-const Assignment = ({assignmentPromise}) => {
+import Buttons from '../Buttons/Buttons';
+import Container from '../container/Container';
+import CardAssignment from '../CardAssignment/CardAssignment';
+import CountBox from '../CountBox/CountBox';
+
+const Assignment = ({ assignmentPromise }) => {
 
   const assignmentData = use(assignmentPromise);
 
-  console.log(assignmentData);
-    
+  const [toggleStatus, setToggleStatus] = useState("All")
 
-    return (
-        <div>
-            {
-              assignmentData.map(assignment => { 
-                console.log(assignment);
-               return (
-                <div className='shadow-md rounded-md cursor-pointer bg-slate-100'>
-                    <div>
-                      <img className='h-18' src={userImg} alt="" />
-                    </div>
-                    <h2>{assignment.requestedBy}</h2>
-                </div>
-               )
-              })
-            }
-        </div>
-    );
+  const [data, setData] = useState(assignmentData)
+
+
+
+  const filterdData = toggleStatus == "All" ? data : data.filter(element => element.status == toggleStatus)
+  console.log(toggleStatus);
+  console.log(filterdData);
+
+  return (
+
+    <div>
+
+    <Container>
+      <CountBox data={data} />
+
+       <Buttons toggleStatus={toggleStatus} setToggleStatus={setToggleStatus}/>
+    </Container>
+
+    
+    <Container>
+
+      {filterdData.length == 0 ? (<h2 className=' text-center text-4xl font-bold text-purple-600 mt-32'> No Data</h2>) :
+
+        <div className='grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 lg:gap-5 md:gap-4 gap-12 p-4 mb-6'>
+        {
+          filterdData.map((assignment, index) =>
+            <CardAssignment assignment={assignment} key={index} data={data} setData={setData} />
+          )
+        }
+      </div>
+      }
+    </Container>
+    </div>
+  );
 };
 
 export default Assignment;
